5d3b37568df3fc69b59889cd91aa459859f44849
codetestTestCode <mmg36003@gmail.com>
2020-10-15 17:13:19 +0630

    first lesson

---
+++ src/lessons1/New2.java
@@ -0,0 +1,17 @@
+package lessons1;
+public class New2 {
+	public static void main(String[] args) {
+		String firstname = "Kaung";
+		String lastname = " Aung";
+		StringBuilder st = new StringBuilder();
+		st.append(firstname);
+	
+		st.append(lastname);
+		System.out.println(st.toString());
+	}
+	
+}
+
+
+
+
