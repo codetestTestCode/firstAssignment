37df014df458ebe69eccc3587db2d4ab2f12f609
codetestTestCode <mmg36003@gmail.com>
2020-10-21 10:18:59 +0630

    Assignment3

---
+++ src/lessons1/Assignment3.java
@@ -0,0 +1,17 @@
+package lessons1;
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		int d = 0;
+        for(int x = 0;x<=10000;x++){
+
+        		x*=10;
+        		d+=x+1;
+
+     System.out.println("result "+ d);
+        }
+
+	}
+
+}
