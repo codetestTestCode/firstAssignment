6ad05850247e7d6053bee4e21b14edcb9fd1342c
codetestTestCode <mmg36003@gmail.com>
2020-10-27 17:01:11 +0630

    fix Asssign5

--- src/lessons1/Assignment5.java
+++ src/lessons1/Assignment5.java
@@ -3,19 +3,15 @@ package lessons1;
 public class Assignment5 {
 
 	public static void main(String[] args) {
-		for(byte i=1;i<=5;i++){
-            for(byte y=i;y<=5;y++){
+		for(byte i=1;i<=9;i+=2){
+            for(byte y=9;y>i;y-=2){
                 System.out.print(" ");
-
             }
-            for(byte x=1 ; x<=(i*2)-1;x++){
-
-                    System.out.print(x);
-            }
-
+           for(byte x=1 ; x<=i;x++){
+              System.out.print(x);
+                }
             System.out.println();
-
-            }
+        }
 
 	}
 
