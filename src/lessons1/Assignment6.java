bec1e25730fa960e91167f99b7324487e19566f0
codetestTestCode <mmg36003@gmail.com>
2020-10-27 20:45:25 +0630

    Assign6

---
+++ src/lessons1/Assignment6.java
@@ -0,0 +1,30 @@
+package lessons1;
+
+import java.util.Scanner;
+
+public class Assignment6 {
+
+	public static void main(String[] args) {
+	
+	        System.out.println("Please Type Number Only!");
+	        Scanner number = new Scanner(System.in);
+	        try{ byte a = number.nextByte();
+
+
+	              for(byte i=a;1<=i;i-=2){
+	                  for(byte y=a;y>i;y-=2){
+	                      System.out.print(" ");
+	                  }
+	                  for(byte x=1 ; x<=i;x++){
+	                      System.out.print(x);
+	                     }
+	                  System.out.println(); }
+
+	          } catch (Exception e) {
+	            System.out.println("Please Try Again Type Number");
+
+	        }
+
+	}
+
+}
