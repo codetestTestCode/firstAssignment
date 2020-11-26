f918704b1ec23cc5445f31520c0de2324ef0af0a
codetestTestCode <mmg36003@gmail.com>
2020-11-26 15:54:20 +0630

    fix Assign8

--- src/lessons1/Assignment8.java
+++ src/lessons1/Assignment8.java
@@ -2,19 +2,17 @@ package lessons1;
 
 public class Assignment8 {
 
-	public static void main(String[] args) {
-		 double deposit = 50;
-	        byte monthlyDeposit = 50;
+	    public static void main(String[] args) {
+	        double deposit = 50;
 	        double interest = 5;
-	        double totalAmount ;
-	        double percentage = 0;
-	        for(byte x=1;x<=12;x++){
-	            totalAmount = deposit += percentage;
-	                   percentage += deposit*interest/100;
-	            deposit += monthlyDeposit;
-	            System.out.println(totalAmount);
-	            }
+	        double totalAmount =0;
+	for(byte x=1;x<=12;x++){
+	    totalAmount += deposit;
+	    deposit += deposit*interest/100;
+	    System.out.println(totalAmount);
+	    }
+	System.out.println();
+	System.out.println("Total :" + totalAmount);
+	    }
+	        }
 
-	}
-
-}
